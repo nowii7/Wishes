77fe58452244fe3f843996fc9973f8b93fcce101
nowii7 <starbd2007@gmail.com>
2026-07-23 21:42:56 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("Mission Started");
+// Create 200 stars
+
+for(let i = 0; i < 200; i++){
+
+    let star = document.createElement("div");
+
+    star.className = "star";
+
+    star.style.left = Math.random()*100 + "vw";
+
+    star.style.top = Math.random()*100 + "vh";
+
+    star.style.animationDelay = Math.random()*3 + "s";
+
+    document.body.appendChild(star);
+
+}
